84d2a084d6b110bc17d460bba32459e19795368d
Jose Gleiser <jose.gleiser@gmail.com>
2013-11-08 19:22:30 -0300

    version 1.0, doesn't use any JS framework

--- README.md
+++ README.md
@@ -3,21 +3,21 @@ Verificar RUT Chileno en JavaScript
 
 Función JavaScript para verificar que el rut ingresado en un input sea correcto,
 además formatea el input ingresado para eliminar cualquier caracter no válido y
-representar el rut en el formato xx.xxx.xxx-x
+representar el rut en el formato xxxxxxxx-x
 
 -----------------
 
 This Javascript function verifies and validates the Chilean RUT. It will delete
-any non valid character and will give it the format xx.xxx.xxx-x
+any non valid character and will give it the format xxxxxxxx-x
 
 -----------------
 
 The algorithm for calculating the check digit was based on
 http://users.dcc.uchile.cl/~mortega/microcodigos/validarrut/algoritmo.html
 
-Version 0.9: Requires jQuery.
+Check example.html to see how to use it, doesn't require any framework.
 
-TO-DO list for version 1.0:
-- Eliminate jQuery dependencies, make it pure JavaScript.
+In a nutshell, `RUT.verificar(inputElement)` returns `true` if is correct, `false`
+if is wrong and `undefined` if inputElement is empty.
 
 Released under MIT License.
--- verificar-rut.js
+++ verificar-rut.js
@@ -1,51 +1,68 @@
+/** Función JavaScript para verificar el RUT chileno el cual también elimina
+  * cualquier caracter no válido.
+  * Creado por José Gleiser (http://www.gleiser.net) y distribuido bajo
+  * licencia MIT.
+  */
 var RUT = window.RUT = window.RUT || {};
-/*
-  Algoritmo para obtener el digito verificador de un rut
-  http://users.dcc.uchile.cl/~mortega/microcodigos/validarrut/algoritmo.html
-*/
+
+/** Algoritmo para obtener el digito verificador de un rut (Algoritmo 1)
+  * http://users.dcc.uchile.cl/~mortega/microcodigos/validarrut/algoritmo.html
+  */
 RUT.digitoVerificador = function (rut) {
-  var s = rut.split('').reverse(), j = 2, t = 0, v = '', l = s.length, i;
+
+  var s = rut.split('').reverse(),
+      j = 2,
+      t = 0,
+      v = '',
+      l = s.length,
+      i;
+
   for (i = 0; i < l; i++) {
     if (8 === j) { j=2; }
     s[i] *= j++;
     t += s[i];
   }
+
   t %= 11;
   t = 11 - t;
-  if (10 === t) { v='K'; }
-  else if (11 === t) { v='0'; }
-  else { v = t.toString(); }
+
+  if (10 === t) v='K';
+  else if (11 === t) v='0';
+  else v = t.toString();
+
   return v;
 };
-/*
-  Verifica que el rut ingresado sea correcto y lo formatea en formato 12345678-9
-  Empresa es boolean, si es true no acepta rut menores a 50 millones
-*/
+
+/** Verifica que el rut ingresado sea correcto y lo deja en formato 12345678-9
+  * Empresa es boolean, si es true no acepta rut menores a 50 millones
+  * Retorna true si es correcto, false si no lo es, undefined si está vacío.
+  */
 RUT.verificar = function (input, empresa) {
-  var
-    $input = $(input),
-    rut = $input.val().replace(/[^kK\d]/g,""), // solo deja nums y k
-    len = rut.length,
-    r = rut.substring(0, len-1).replace(/[kK]/g,""), // sin digito verificador y quita las k
-    d = rut.substring(len-1).toUpperCase(), // digito verificador
-    v;
+  var rut = input.value.replace(/[^kK\d]/g, ''), // solo deja nums y k
+      len = rut.length,
+      r = rut.substring(0, len-1).replace(/[kK]/g, ''), // sin digito verificador y quita las k
+      d = rut.substring(len-1).toUpperCase(), // digito verificador
+      valid,
+      v;
+
   len = r.length;
+
   if (0 < len) {
     v = RUT.digitoVerificador(r);
     r = r.substring(len-9, len);
-    $input.val(r + '-' + d);
+    input.value = r + '-' + d;
+
     if (v !== d) {
-      $input.addClass('input-error').focus();
+      valid = false;
     } else if (empresa && 50000000 > parseInt(r, 10)) {
-      $input.addClass('input-error').focus();
-    } else if ($input.hasClass('input-error')) {
-      $input.removeClass('input-error');
+      valid = false;
+    } else {
+      valid = true;
     }
+
+  } else {
+    input.value = '';
   }
-  else {
-    $input.val('');
-    if($input.hasClass('input-error')) {
-      $input.removeClass('input-error');
-    }
-  }
+  
+  return valid;
 };
